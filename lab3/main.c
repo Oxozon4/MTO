72de8942296c85be3c491509dffb5e5771795117
Oxozon <oxozon.o4@gmail.com>
2022-10-23 10:49:02 +0200

    add another if condition

--- lab3/main.c
+++ lab3/main.c
@@ -47,6 +47,35 @@ int my_printf(char *format_string, char *param){
 				strIterator++;
 			}
 			i += kCharIndex;
+		} else if ((format_string[i] == '#') && (format_string[i+1] == '.') && (isdigit(format_string[i+2]))) {
+			int kCharIndex = 0;
+			for (int numberParamIterator = i + 2; numberParamIterator < strlen(format_string); numberParamIterator++) {
+				if (format_string[numberParamIterator] == 'k') {
+					kCharIndex = numberParamIterator;
+				} else if (!isdigit(format_string[numberParamIterator])) {
+					break;
+				}
+			}
+			if (kCharIndex == 0) {
+				putchar(format_string[i]);
+				return 0;
+			}
+			char numberArray[kCharIndex + 1];
+			strncpy(numberArray, &format_string[i + 2], kCharIndex);
+			numberArray[kCharIndex] = '\0';
+			int number = atoi(numberArray);
+			int strIterator = 0;
+			while (strIterator < strlen(param) && strIterator < number) {
+				if (islower(param[strIterator])) {
+					param[strIterator] = toupper(param[strIterator]);
+				} 
+				else if (isupper(param[strIterator])) {
+					param[strIterator] = tolower(param[strIterator]);
+				}
+				putchar(param[strIterator]);
+				strIterator++;
+			}
+			i += kCharIndex;
 		} else {
 			putchar(format_string[i]);
 		}
