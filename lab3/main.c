df9ab9b9ecbb89f9e20bd0bc0f7a621ceb8c6dc4
Oxozon <oxozon.o4@gmail.com>
2022-10-23 11:40:31 +0200

    add comment

--- lab3/main.c
+++ lab3/main.c
@@ -6,6 +6,7 @@
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
 		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
+			// #k (%s)
 			i++;
 			int strIterator = 0;
 			while (strIterator < strlen(param)) {
