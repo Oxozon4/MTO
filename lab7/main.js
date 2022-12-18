c07f02f8d61f960f60ea24473a2e5dfbf1dd9779
Oxozon <oxozon.o4@gmail.com>
2022-12-18 10:48:51 +0100

    „addHexRegex”

--- lab7/main.js
+++ lab7/main.js
@@ -13,6 +13,8 @@ function my_printf(format_string, param) {
       format_string.charAt(i) === '#' &&
       format_string.charAt(i + 1) === 'j'
     ) {
+			const regex = var regex = /[0-9A-Fa-f]{6}/g;
+			if ()
     } else {
       process.stdout.write(format_string.charAt(i));
     }
