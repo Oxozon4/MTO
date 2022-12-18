32437d355bfcf4490cf40b9d633c8232b77bff85
Oxozon <oxozon.o4@gmail.com>
2022-12-18 11:21:41 +0100

    „fixSolutionForUpperCase”

--- lab7/main.js
+++ lab7/main.js
@@ -15,12 +15,12 @@ function my_printf(format_string, param) {
     ) {
       if (!isNaN(param)) {
         const hexOutput = parseInt(param).toString(16);
-        hexOutput.replaceAll('a', 'g');
-        hexOutput.replaceAll('b', 'h');
-        hexOutput.replaceAll('c', 'i');
-        hexOutput.replaceAll('d', 'j');
-        hexOutput.replaceAll('e', 'k');
-        hexOutput.replaceAll('f', 'l');
+        hexOutput.replace(/a/gi, 'g');
+        hexOutput.replace(/b/gi, 'h');
+        hexOutput.replace(/c/gi, 'i');
+        hexOutput.replace(/d/gi, 'j');
+        hexOutput.replace(/e/gi, 'k');
+        hexOutput.replace(/f/gi, 'l');
         process.stdout.write(hexOutput);
       }
       i++;
