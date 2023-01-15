2c6f7d75188cea70fb2817fdd6503d4cccb6fdf9
Oxozon4 <oxozon.o4@gmail.com>
2023-01-15 10:35:18 +0100

    add solution from lab7

--- lab8/main.js
+++ lab8/main.js
@@ -9,6 +9,21 @@ function my_printf(format_string, param) {
     if (format_string.charAt(i) == '#' && format_string.charAt(i + 1) == 'k') {
       process.stdout.write(param);
       i++;
+    } else if (
+      format_string.charAt(i) === '#' &&
+      format_string.charAt(i + 1) === 'j'
+    ) {
+      if (!isNaN(param) && Number.isInteger(Number(param))) {
+        const hexOutput = parseInt(param).toString(16);
+        hexOutput.replace(/a/gi, 'g');
+        hexOutput.replace(/b/gi, 'h');
+        hexOutput.replace(/c/gi, 'i');
+        hexOutput.replace(/d/gi, 'j');
+        hexOutput.replace(/e/gi, 'k');
+        hexOutput.replace(/f/gi, 'l');
+        process.stdout.write(hexOutput);
+      }
+      i++;
     } else {
       process.stdout.write(format_string.charAt(i));
     }
@@ -22,7 +37,11 @@ process.stdin.on('data', function (chunk) {
   lines[0] = lingeringLine + lines[0];
   lingeringLine = lines.pop();
   for (var i = 0; i < lines.length; i++) {
-    my_printf(lines[i], lines[i + 1]);
+    try {
+      my_printf(lines[i], lines[i + 1]);
+    } catch (e) {
+      // error
+    }
     i++;
   }
 });
